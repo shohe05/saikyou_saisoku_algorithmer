c7ab57cb03f40ab907c5e32dd8111d3ee3c8bd83
Syouhei Tanaka <shou.tanak@gmail.com>
2015-03-16 17:48:23 +0900

    Implement InterestingParty

---
+++ chapter5_full_search/interesting_party.py
@@ -0,0 +1,14 @@
+class InterestingParty:
+    def best_invitation(self, first, second):
+        all_hobbies = first + second
+        hobby_popularity = {}
+        for hobby in all_hobbies:
+            hobby_popularity[hobby] = hobby_popularity.get(hobby, 0)
+            hobby_popularity[hobby] += 1
+        return max(hobby_popularity.values())
+
+if __name__ == "__main__":
+    interesting_party = InterestingParty()
+    first = ['sn', 'pr', 'co', 'mo']
+    second = ['py', 'py', 'an', 'py']
+    print(interesting_party.best_invitation(first, second))
